ef62a252437957b1ddf0793cf696166bd2a411ad
Meirasulin <asulinmeir64@gmail.com>
2024-09-29 21:07:18 +0300

    initial calculator class with calculator opertion functions

---
+++ Calculator/CalculatorClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculation
+{
+    class Calculator
+    {
+        static int Pluse(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        static int Minus(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        static int Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
